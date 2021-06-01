5f3008a380db8bd1967c6a9255c9eed920f74b6b
Benjamin Greif <b.greif@mailbox.org>
2021-06-01 13:14:03 +0200

    adjusted movie frontend

---
+++ src/App.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import React from 'react';
+
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+               Hello World
+            </div>
+        );
+    }
+}
+
