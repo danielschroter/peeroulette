ba11997375a6ffdb070116214205baf04c9a88eb
Daniel Schroter <daniel.schroter@tum.de>
2021-06-06 19:48:55 +0200

    Landig Page Showing Placeholder

--- src/views/LandingPageView.js
+++ src/views/LandingPageView.js
@@ -1,18 +1,32 @@
-import React  from "react";
-import { connect } from "react-redux";
-
+import React from "react";
+import {connect} from "react-redux";
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 /**
  * Manages the process of getting movie list data
  * @param {props} props
  */
-function MovieListView(props) {
+function LandingPageView(props) {
 
     return (
         // if no movies are loaded, the above useEffect should be triggered
-        <div>Hello Peeroulette</div>
+        <div style={{'marginTop': '150px', 'marginBottom': '150px'}}>
+            <Container maxWidth="sm">
+
+                <Typography variant="h4" align="center" gutterBottom>
+                    Hello Peeroulette
+                </Typography>
+
+
+                <Typography variant="body1" align="center" paragraph>
+                    Comming soon...
+                </Typography>
+
+            </Container>
+        </div>
     );
 }
 
 // connect() establishes the connection to the redux functionalities
-export default connect()(MovieListView);
+export default connect()(LandingPageView);
