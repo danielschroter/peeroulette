dcb8e4ea6821315b30ea9da0650aa0c52ff57f9a
Benjamin Greif <b.greif@mailbox.org>
2021-07-01 19:51:27 +0200

    removed fb importt

--- src/components/EditProfileComponent.js
+++ src/components/EditProfileComponent.js
@@ -16,7 +16,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import {facebookInterests} from '../components/FacebookInterests';
 
 
 const useStyles = makeStyles((theme) => ({
