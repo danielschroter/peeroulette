b1f6c7305f475387ddb0eb093b3b9ae0f4086a20
Daniel Schroter <daniel.schroter@tum.de>
2021-07-19 15:29:32 +0200

    Fix: Have possible Interests in Frontend to avoid random bug...

--- src/components/MyCalendarComponent.js
+++ src/components/MyCalendarComponent.js
@@ -7,6 +7,7 @@ import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import Chip from "@material-ui/core/Chip";
+
 import {
     Scheduler,
     Toolbar,
@@ -24,7 +25,9 @@ import {
 import {withStyles} from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import UserService from "../services/UserService";
+import MatchService from "../services/MatchService";
 import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 
 const style = ({palette}) => ({
@@ -65,27 +68,49 @@ const TextEditor = (props) => {
 
 const BasicLayout = ({onFieldChange, appointmentData, ...restProps}) => {
 
-    const values = ["hello", "world", "was ", "ist ", "denn ", "das ", "für ", "ein ", "Selektor"];
+    // const [allInterests, setAllInterests] = React.useState([]);
 
-    const currencies = [
-        {
-            value: 'USD',
-            label: '$',
-        },
-        {
-            value: 'EUR',
-            label: '€',
-        },
-        {
-            value: 'BTC',
-            label: '฿',
-        },
-        {
-            value: 'JPY',
-            label: '¥',
-        },
+    const allInterests = ["Business and Industry","Advertising","Agriculture","Architecture","Aviation","Banking","Investment banking",
+        "Online banking","Retail banking","Business","Construction","Design","Fashion design","Graphic design","Interior design","Economics","Engineering", "Entrepreneurship", "Health care",
+        "Higher education", "Management", "Marketing", "Nursing", "Online","Digital marketing",
+        "Display advertising", "Email marketing", "Online advertising", "Search engine optimization", "Social media", "Social media marketing",
+        "Web design", "Web development", "Web hosting", "Personal finance", "Creditcards", "Insurance", "Investment", "Mortgage loans", "Real estate",
+        "Retail", "Sales", "Science", "Small business", "Entertainment", "Games", "Action games", "Board games",
+        "Browser games", "Card games", "Casino games", "First-person shooter games", "Gambling", "Massively multiplayer online games", "Massively multiplayer online role-playing games",
+        "Online games", "Online poker", "Puzzle video games", "Racing games", "Role-playing games", "Shooter games", "Simulation games", "Sports games", "Strategy games", "Video games",
+        "Word games", "Live events", "Ballet", "Bars", "Concerts", "Dancehalls", "Music festivals", "Nightclubs", "Parties", "Plays", "Theatre", "Movies", "Action movies",
+        "Animated movies", "Anime movies", "Bollywood movies", "Comedy movies", "Documentary movies", "Drama movies", "Fantasy movies", "Horror movies", "Musical theatre", "Science fiction movies",
+        "Thriller movies", "Music", "Blues music","Classical music", "Country music", "Dance music", "Electronic music", "Gospel music",
+        "Heavy metal music", "Hip hop music", "Jazz music", "Music videos", "Pop music", "Rhythm and blues music", "Rock musicSoul music", "Reading", "Books",
+        "Comics", "E-books", "Fiction books", "Literature", "Magazines", "Manga", "Mystery fiction", "Newspapers",
+        "Non-fiction books", "Romance novels", "TV", "TV comedies", "TV game shows", "TV reality shows", "TV talkshows", "Family and relationships",
+        "Dating", "Family", "Fatherhood", "Friendship", "Marriage", "Motherhood", "Parenting", "Weddings", "Fitness and wellness",
+        "Bodybuilding", "Meditation", "Physical exercise", "Physical fitness", "Running", "Weight training", "Yoga", "Food and drink", "Alcoholic beverages", "Beer",
+        "Distilled beverage", "Wine", "Beverages", "Coffee", "Energy drinks", "Juice", "Soft drinks", "Tea", "Cooking", "Baking",
+        "Recipes", "Cuisine", "Chinese cuisine", "French cuisine", "German cuisine", "Greek cuisine", "Indian cuisine", "Italian cuisine", "Japanese cuisine", "Korean cuisine", "Latin American cuisine",
+        "Mexican cuisine", "Middle Eastern cuisine", "Spanish cuisine", "Thai cuisine", "Vietnamese cuisine", "Food", "Barbecue", "Chocolate", "Desserts", "Fast food",
+        "Organic food", "Pizza", "Seafood", "Veganism", "Vegetarianism", "Restaurants", "Coffeehouses",
+        "Diners", "Fast casual restaurants", "Fast food restaurants", "Hobbies and activities", "Arts and music", "Acting",
+        "Crafts", "Dance","Drawing", "Drums", "Fine art", "Guitar", "Painting", "Performing arts", "Photography","Sculpture", "Singing", "Writing", "Current events",
+        "Home and garden", "Do it yourself (DIY)", "Furniture", "Gardening", "Home Appliances", "Home improvement", "Pets", "Birds", "Cats", "Dogs", "Fish", "Horses", "Pet food",
+        "Rabbits", "Reptiles", "Politics and social issues","Charity and causes", "Community issues",
+        "Environmentalism","Law", "Military","Politics", "Religion","Sustainability", "Veterans","Volunteering", "Travel", "Adventure travel","Air travel","Beaches",
+        "Car rentals", "Cruises", "Ecotourism", "Hotels", "Lakes", "Mountains", "Nature", "Theme parks", "Tourism", "Vacations",
+        "Vehicles", "Automobiles", "Boats", "Electric vehicle", "Hybrids", "Minivans", "Motorcycles", "RVs", "SUVs", "Scooters", "Trucks", "Shopping and fashion", "Beauty", "Beauty salons", "Cosmetics", "Fragrances",
+        "Hair products", "Spas", "Tattoos", "Clothing", "Children’s clothing", "Men’s clothing", "Shoes", "Women’s clothing", "Fashionaccessories", "Dresses", "Handbags", "Jewelry",
+        "Sunglasses","Shopping", "Boutiques", "Coupons", "Discount stores", "Luxury goods", "Online shopping", "Shopping malls", "Toys", "Sports and outdoors", "Outdoor recreation", "Boating", "Camping", "Fishing",
+        "Horseback riding", "Hunting", "Mountain biking", "Surfing", "Sports", "American football", "Association football (Soccer)", "Auto racing", "Baseball", "Basketball", "College football", "Golf", "Marathons",
+        "Skiing", "Snowboarding","Swimming", "Tennis", "Thriathlons", "Volleyball", "Technology", "Computers", "Computer memory", "Computer monitors", "Computer processors", "Computer servers", "Desktop computers", "Free software", "Hard drives", "Network storage", "Software", "Tablet computers",
+        "Consumer electronics","Audio equipment", "Camcorders", "Cameras", "E-book readers", "GPS devices", "Game consoles", "Mobile phones", "Portable media players", "Projectors", "Smartphones", "Televisions",
     ];
-
+    // const extractInts = async() => {
+    //     let ints = await MatchService.getInterests();
+    //     setAllInterests(ints[0].facebookInterests);
+    // }
+    //
+    // useEffect(() => {
+    //    extractInts();
+    // }, []);
 
     const onURLFieldChange = (nextValue) => {
         onFieldChange({link: nextValue})
@@ -133,10 +158,9 @@ const BasicLayout = ({onFieldChange, appointmentData, ...restProps}) => {
             />
 
             <Autocomplete
-                style={{'marginTop':'10px'}}
                 multiple
                 id="tags-outlined"
-                options={values}
+                options={allInterests}
                 getOptionLabel={(option) => option}
                 defaultValue={appointmentData.interests}
                 filterSelectedOptions
@@ -145,8 +169,8 @@ const BasicLayout = ({onFieldChange, appointmentData, ...restProps}) => {
                     <TextField
                         {...params}
                         variant="outlined"
-                        label="Your Interests"
-                        placeholder="Your Labels"
+                        label="Interests"
+                        placeholder="Your Talk Tags"
                     />
                 )}
             />
@@ -155,41 +179,19 @@ const BasicLayout = ({onFieldChange, appointmentData, ...restProps}) => {
     );
 };
 
-const appointments = [
-    {
-        id: 234234,
-        startDate: '2021-07-13T10:45',
-        endDate: '2021-07-13T12:45',
-        title: 'Gym',
-        link: "Jitsi URL1",
-        description: "Get ready in the gym for summer"
-    },
-    {
-        id: 12312323,
-        startDate: '2021-07-13T17:30',
-        endDate: '2021-07-13T19:30',
-        title: 'DeepL Exam',
-        link: "Jitsi URL2",
-        description: "Make an Exam in DeepLearning Test"
-    },
-];
-
-
 
 export default class MyCalendarComponent extends React.PureComponent {
-
 
 
     constructor(props) {
         super(props);
         this.state = {
-            data: appointments,
+            data: [],
             currentDate: '2021-07-13',
             addedAppointment: {},
             appointmentChanges: {},
             editingAppointment: undefined,
             user: props.user,
-            interests: ["was", "für"],
             mappings: {},
         };
         this.currentDataChange = (currentDate) => {
@@ -209,32 +211,18 @@ export default class MyCalendarComponent extends React.PureComponent {
         description: appointment.description,
         link: appointment.link,
         user: appointment.user,
-        interests:["was", "für"],
+        interests:appointment.interests,
     });
 
     async componentDidMount() {
         let response = await AppointmentService.getAppointments();
         let apps = response.appointments;
         let mapping = response.mapping;
-        // for (var i in response){
-        //     try{
-        //         const u = await UserService.getUser('' + response[i].user);
-        //         console.log("u " + u);
-        //     }catch (e) {
-        //
-        //     }
-        //
-        // }
-        console.log("vorher" + mapping[apps[0].user]);
-        // const val =  mapping[apps[0].user];
-        // apps[0].user = val;
-        console.log("nachher" + mapping[apps[0].user]);
+
         let res_mapped = apps.map(this.mapAppointmentData, mapping);
-        // for (var i in res_mapped){
-        //     const val = mapping[apps[i].user];
-        //     res_mapped[i].user = val;
-        // }
-        this.setState({data: res_mapped, mapping: mapping});
+
+        let allInterests = await MatchService.getInterests()
+        this.setState({data: res_mapped, mapping: mapping, allInterests: allInterests[0].facebookInterests});
     }
 
 
@@ -332,6 +320,12 @@ export default class MyCalendarComponent extends React.PureComponent {
                     <Grid item xs={10}>
                         <span>{this.state.mapping[appointmentData.user]}</span>
                     </Grid>
+                    <Grid item xs={2} className={classes.textCenter}>
+                        <span>Tags: </span>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <span>{appointmentData.interests.map(app => app + ", ")}</span>
+                    </Grid>
                 </Grid>
             </AppointmentTooltip.Content>
         ));
@@ -373,7 +367,7 @@ export default class MyCalendarComponent extends React.PureComponent {
                     />
                     <AppointmentForm
                     basicLayoutComponent={BasicLayout}
-                    // textEditorComponent={TextEditor}
+                    textEditorComponent={TextEditor}
                     messages={messages}/>
                 </Scheduler>
             </Paper>
